6b7281c36c2cfe98675a12a19fece25d4d4d46fe
rlawnsgh78 <rlawnsgh78@gimjunhoui-MacBook-Pro.local>
2016-11-20 21:00:15 +0900

    회원가입 구현중
    회원가입 구현중

--- app/src/main/java/com/example/rlawnsgh78/chatapp/RegisterActivity.java
+++ app/src/main/java/com/example/rlawnsgh78/chatapp/RegisterActivity.java
@@ -2,6 +2,10 @@ package com.example.rlawnsgh78.chatapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +13,45 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        MaterialEditText editRegisterId = (MaterialEditText) findViewById(R.id.edit_Register_Id);
+        editRegisterId.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        MaterialEditText editRegisterPassword = (MaterialEditText) findViewById(R.id.edit_Register_Password);
+
+
+        MaterialEditText editRegisterNickname = (MaterialEditText) findViewById(R.id.edit_Register_Nickname);
+        editRegisterNickname.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
+
